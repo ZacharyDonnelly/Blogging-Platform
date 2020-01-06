c20873c3a720f9d7511dc7cf928e48b7b36ac7f8
Hashed <z-donnelly@hvcc.edu>
2020-01-06 03:18:11 -0500

    new branch, setup folder structure in src, add store file, change main -> index

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,8 @@
+/* eslint-disable no-undef */
 const path = require("path");
 
 module.exports = {
-  entry: "./src/main.js",
+  entry: "./src/index.js",
   output: {
     filename: "./bundle.js",
     path: path.resolve(__dirname, "dist") // base path where to send compiled assets
