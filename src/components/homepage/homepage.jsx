087f26876c13ead56351a6f98542c50cb9de2021
Hashed <z-donnelly@hvcc.edu>
2020-01-06 21:56:03 -0500

    add homepage styling, with lorem ipsum

--- src/components/homepage/homepage.jsx
+++ src/components/homepage/homepage.jsx
@@ -1,7 +1,42 @@
 import React from "react";
+import "./homepage.scss";
 
 const HomePage = () => {
-  return <div></div>;
+  return (
+    <div className="wrapper">
+      <div className="box header">
+        <ul>
+          <li>About</li>
+          <li>Blog</li>
+          <li>Projects</li>
+          <li>Contact</li>
+        </ul>
+      </div>
+      <div className="box sidebar">
+        <h1>
+          Zach <span>Donnell</span>y
+        </h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Felis
+          donec et odio pellentesque diam volutpat. Sed egestas egestas
+          fringilla phasellus faucibus. Amet tellus cras adipiscing enim. Tellus
+          molestie nunc non blandit massa enim nec. Faucibus et molestie ac
+          feugiat sed lectus. Gravida rutrum quisque non tellus orci ac.
+        </p>
+
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Felis
+          donec et odio pellentesque diam volutpat. Sed egestas egestas
+          fringilla phasellus faucibus. Amet tellus cras adipiscing enim. Tellus
+          molestie nunc non blandit massa enim nec. Faucibus et molestie ac
+          feugiat sed lectus. Gravida rutrum quisque non tellus orci ac.
+        </p>
+      </div>
+      <div className="box content"></div>
+    </div>
+  );
 };
 
 export default HomePage;
