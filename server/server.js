156571cd02718f0cb8e752be5bc7cd524cb9eec4
Hashed <z-donnelly@hvcc.edu>
2020-01-09 03:12:11 -0500

    implement JWT/bcrypt with mongoDB

--- server/server.js
+++ server/server.js
@@ -1,21 +1,19 @@
 import express from "express";
-import mongoose from "mongoose";
 import cors from "cors";
 import { ApolloServer } from "apollo-server-express";
 import graphqlHTTP from "express-graphql";
+import mongoose from "mongoose";
 import bcrypt from "bcrypt";
-import { schema } from "./schema";
-import settings from "./settings";
 import jwt from "jsonwebtoken";
 
-import UserSchema from "./schemas/users";
+import { schema } from "./schema";
+
+import settings from "./config/settings";
+
+import UserSchema from "./schemas/user";
 
 const app = express();
 
-// // // eslint-disable-next-line no-undef
-// // const MongoClient = require("mongodb").MongoClient;
-
-// const client = new MongoClient(uri, { useNewUrlParser: true });
 mongoose.connect(settings.uri, { useNewUrlParser: true });
 
 app.use(express.urlencoded());
@@ -36,27 +34,61 @@ app.use(
     graphiql: true
   })
 );
-app.post("/user", async ({ body }, res) => {
+const server = new ApolloServer({ schema });
+server.applyMiddleware({ app });
+
+const validateAuth = (req, res, next) => {
+  const authHeader = req.headers["authorization"];
+  if (typeof authHeader === "undefined" && !authHeader.includes("Bearer")) {
+    res.status(403);
+    res.send({ error: "invalid creds" });
+    return;
+  }
+
+  const bearer = authHeader.split("Bearer")[1];
+
+  try {
+    const decoded = jwt.verify(bearer.replace(" ", ""), "changeME");
+  } catch (err) {
+    res.status(405);
+    res.send({ error: "invalid creds" });
+
+    return;
+  }
+
+  next();
+};
+
+app.get("/thing", validateAuth, (req, res) => {
+  res.send("Working!!");
+});
+
+app.post("/users", async ({ body }, res) => {
   bcrypt.hash(body.password, 10, async (err, hash) => {
     const user = new UserSchema({ ...body, password: hash });
     await user.save();
   });
 
+  res.status(200);
+});
+
+app.post("/auth", async ({ body }, res) => {
+  const { password: hashedPassword } = await UserSchema.findOne({
+    email: body.email
+  });
+
+  const doesMatch = await bcrypt.compare(body.password, hashedPassword);
+  if (!doesMatch) {
+    res.status(403);
+    res.send({ error: "invalid password" });
+    return;
+  }
+
   res.send({
-    token: jwt.sign({ email: body.email, issuer: "dis" }, "replace later", {
-      algorithm: "RS256"
-    }),
+    token: jwt.sign({ email: body.email, issuer: "dis" }, "changeME"),
     iat: ~~(new Date() / 1000)
   });
 });
-
-app.post("/auth", async ({ body: { email, pasword } }) => {
-  const user = await new UserSchema().findOne({ email });
-  console.log("sdfsdf", user);
-});
-
-const server = new ApolloServer({ schema });
-server.applyMiddleware({ app });
 
 app.listen({ port: 3006 }, () =>
   console.log("Now browse to http://localhost:3006" + server.graphqlPath)
