8a8f0acea2a95f1c47505583d9d47362a3137804
Hashed <z-donnelly@hvcc.edu>
2020-01-09 21:45:37 -0500

    set up click handler for axios post

--- server/server.js
+++ server/server.js
@@ -2,10 +2,16 @@ import express from "express";
 import cors from "cors";
 import { ApolloServer } from "apollo-server-express";
 import graphqlHTTP from "express-graphql";
-
-import { schema } from "./schema";
+import mongoose from "mongoose";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import schema from "./schemas/gqlSchema";
+import { MONGO_URI } from "./config/settings";
+import UserSchema from "./schemas/users";
+import Axios from "axios";
 
 const app = express();
+mongoose.connect(MONGO_URI, { useNewUrlParser: true });
 
 app.use(express.urlencoded());
 app.use(express.json());
@@ -27,6 +33,70 @@ app.use(
 );
 const server = new ApolloServer({ schema });
 server.applyMiddleware({ app });
+
+//provide a jwt token
+//make post call
+// is res.status === 200
+// Axios.post(url, {body: email,password}){
+//   if(){
+//     history.push('/blog')
+//   }
+// }
+
+const validateAuth = (req, res, next) => {
+  // expecting an authorization header
+  const authHeader = req.headers["authorization"];
+  if (typeof authHeader === "undefined" && !authHeader.includes("Bearer")) {
+    res.status(403);
+    res.send({ error: "invalid creds" });
+    return;
+  }
+
+  const bearer = authHeader.split("Bearer")[1];
+
+  try {
+    const decoded = jwt.verify(bearer.replace(" ", ""), "changeME");
+  } catch (err) {
+    res.status(405);
+    res.send({ error: "invalid creds" });
+
+    return;
+  }
+
+  next();
+};
+
+app.get("/thing", validateAuth, (req, res) => {
+  res.send("Working!!");
+});
+
+app.post("/users", async ({ body }, res) => {
+  bcrypt.hash(body.password, 10, async (err, hash) => {
+    const user = new UserSchema({ ...body, password: hash });
+    await user.save();
+  });
+
+  res.status(200);
+  res.send({ created: "success" });
+});
+
+app.post("/auth", async ({ body }, res) => {
+  const { password: hashedPassword } = await UserSchema.findOne({
+    email: body.email
+  });
+
+  const doesMatch = await bcrypt.compare(body.password, hashedPassword);
+  if (!doesMatch) {
+    res.status(403);
+    res.send({ error: "invalid password" });
+    return;
+  }
+
+  res.send({
+    token: jwt.sign({ email: body.email, issuer: "dis" }, "changeME"),
+    iat: ~~(new Date() / 1000)
+  });
+});
 
 app.listen({ port: 3006 }, () =>
   console.log("Now browse to http://localhost:3006" + server.graphqlPath)
