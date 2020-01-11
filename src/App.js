1bf38a8a11568336db4b78df7ae1e9e95eeec070
Hashed <z-donnelly@hvcc.edu>
2020-01-10 19:47:14 -0500

    wrapped routes with the apollo client so clientside has access

--- src/App.js
+++ src/App.js
@@ -9,18 +9,23 @@ import Signup from "./components/signup/signup-page";
 
 import configureStore from "./store/configureStore";
 import ProfilePage from "./components/profile/profile-page";
+import { ApolloProvider } from "react-apollo";
+import ApolloClient from "apollo-boost";
 
+const client = new ApolloClient({ uri: "http://localhost:3006/graphql" });
 const store = configureStore();
 
 const App = () => (
   <Provider store={store}>
-    <BrowserRouter>
-      <Route path="/" exact component={HomePage} />
-      <Route path="/blog" exact component={BlogLanding} />
-      <Route path="/login" exact component={Login} />
-      <Route path="/signup" exact component={Signup} />
-      <Route path="/profile" exact component={ProfilePage} />
-    </BrowserRouter>
+    <ApolloProvider client={client}>
+      <BrowserRouter>
+        <Route path="/" exact component={HomePage} />
+        <Route path="/blog" exact component={BlogLanding} />
+        <Route path="/login" exact component={Login} />
+        <Route path="/signup" exact component={Signup} />
+        <Route path="/profile" exact component={ProfilePage} />
+      </BrowserRouter>
+    </ApolloProvider>
   </Provider>
 );
 
