89896ce53ccb9746b4221e8fc61b1674ff570ccf
parxyHash <54864201+parxyHash@users.noreply.github.com>
2020-01-11 21:03:14 -0500

    Feat frontend (#13)
    * changes to navbar on /blog
    
    * assests name changes
    
    * add more styling fixed a few bugs
    
    * finished a good chunk of the BASIC layout of /blog doing some bug fixes now
    
    * last commit for the night, readded labels to sign in/sign up page
    
    * change blog main photo name, more styling to page, add apollo client to routes

--- src/components/blog-landing/blog-landing.js
+++ src/components/blog-landing/blog-landing.js
@@ -1,18 +1,41 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+
+import photo from "../../../public/assets/PS-Blog.png";
+
 import "./blog-landing.scss";
 
-const BlogMain = () => {
+const BlogLanding = () => {
   return (
-    <>
-      <div className="nav-div">
-        <nav className="blog-nav">
+    <div className="full">
+      <nav className="blog-nav">
+        <div className="clearfix">
           <ul className="blog-list-left">
             <Link to="/" className="blog-link">
               Home
             </Link>
           </ul>
+          <ul className="blog-list-right">
+            <Link to="/profile" className="blog-link">
+              Profile
+            </Link>
+          </ul>
+        </div>
+      </nav>
+      <h2 className="nav-center">Bloggr</h2>
+      <div className="blog-wrapper">
+        <div className="blog-container">
+          <div className="sidebar-container">
+            <h2 className="sidebar-header">Header</h2>
+            <p className="sidebar-text">
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sit
+              impedit commodi excepturi nulla aliquam quia autem vero ut.
+              Aliquam eius soluta nobis magnam qui dolor fugit natus obcaecati
+              cupiditate nostrum, reprehenderit et consectetur. Est earum
+              dolorum porro facilis fugit quidem nemo aliquam dignissimos. Ex,
+              eligendi incidunt corrupti ducimus illum magni.
+            </p>
           <div className="clearfix">
             <ul className="blog-list-right">
               <Link to="/profile" className="blog-link">
@@ -20,18 +43,11 @@ const BlogMain = () => {
               </Link>
             </ul>
           </div>
-        </nav>
-        <h2 className="nav-center">Bloggr</h2>
+        </div>
+        <img src={photo} className="blog-img"></img>
       </div>
-      <div className="wrapper">
-        <img
-          className="top-img"
-          src="https://burst.shopifycdn.com/photos/laptop-on-desk-from-above_925x.jpg"
-          alt="Laptop and Coffee"
-        />
-      </div>
-    </>
+    </div>
   );
 };
 
-export default BlogMain;
+export default BlogLanding;
